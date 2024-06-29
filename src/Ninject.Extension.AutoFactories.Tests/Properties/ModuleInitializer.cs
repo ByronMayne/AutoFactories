ddcb4ccea5dc8e91fcb30be1df196f3805c35857
Byron Mayne <Byronmayne@gmail.com>
2024-06-29 18:40:15 -0400

    Removed the example project Sandbox and create a basic unit test framework

---
+++ src/Ninject.Extension.AutoFactories.Tests/Properties/ModuleInitializer.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.CompilerServices;
+using VerifyTests;
+
+namespace Ninject.AutoFactories.Properties;
+
+public static class ModuleInitializer
+{
+    [ModuleInitializer]
+    public static void Init()
+    {
+        VerifySourceGenerators.Initialize();
+    }
+}
---
+++ src/Ninject.Extension.AutoFactories.Tests/SourceGeneratorTestBase.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ninject.AutoFactories
+{
+    /// <summary>
+    /// A unit test base class for writing tests for Source Generators.
+    /// </summary>
+    internal abstract class SourceGeneratorTestBase
+    {
+        /// <summary>
+        /// Verifies that the source sent in here generates into the expected types.
+        /// </summary>
+        /// <param name="source">The source to try to run the source generator on</param>
+        /// <returns>A task to await on</returns>
+        protected static Task Verify(string source)
+        {
+            SyntaxTree syntaxTree = CSharpSyntaxTree.ParseText(source);
+
+            CSharpCompilation compilation = CSharpCompilation.Create(
+                assemblyName: "AutoFactoriesTests",
+                syntaxTrees: new[] { syntaxTree });
+
+            // The 'hoist' is the SGF libraries wrapper around source generators
+            AutoFactorySourceGeneratorHoist generator = new AutoFactorySourceGeneratorHoist();
+
+            GeneratorDriver driver = CSharpGeneratorDriver.Create(generator);
+
+            driver = driver.RunGenerators(compilation);
+
+            return Verifier.Verify(driver);
+        }
+    }
+}
