eab63977c973fe620a00c38948d4bb9e272a6e5b
Byron Mayne <Byronmayne@gmail.com>
2024-06-29 19:59:41 -0400

    Added one basic unit test

--- src/Ninject.Extension.AutoFactories.Tests/SourceGeneratorTestBase.cs
+++ src/Ninject.Extension.AutoFactories.Tests/SourceGeneratorTestBase.cs
@@ -5,20 +5,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit.Abstractions;
 
 namespace Ninject.AutoFactories
 {
     /// <summary>
     /// A unit test base class for writing tests for Source Generators.
     /// </summary>
-    internal abstract class SourceGeneratorTestBase
+    public abstract class SourceGeneratorTestBase
     {
+        protected ITestOutputHelper m_outputHelper;
+
+        protected SourceGeneratorTestBase(ITestOutputHelper outputHelper)
+        {
+            m_outputHelper = outputHelper;
+        }
+
+        /// <summary>
+        /// Writes a line of text that will show up in the output of the unit test
+        /// </summary>
+        /// <param name="text">The text to write</param>
+        protected void WriteLine(string text)
+            => m_outputHelper.WriteLine(text);
+
         /// <summary>
         /// Verifies that the source sent in here generates into the expected types.
         /// </summary>
         /// <param name="source">The source to try to run the source generator on</param>
         /// <returns>A task to await on</returns>
-        protected static Task Verify(string source)
+        protected static Task Compose(
+            string? source = "", 
+            Predicate<string>[]? filters = null)
         {
             SyntaxTree syntaxTree = CSharpSyntaxTree.ParseText(source);
 
@@ -33,7 +50,18 @@ namespace Ninject.AutoFactories
 
             driver = driver.RunGenerators(compilation);
 
-            return Verifier.Verify(driver);
+            VerifySettings settings = new VerifySettings();
+            settings.UseDirectory("Snapshots");
+
+            GeneratorDriverResultFilter filter = new GeneratorDriverResultFilter(driver.GetRunResult(), filters);
+
+            return Verifier.Verify(filter, settings);
+        }
+
+        protected static class Filters
+        {
+            public static Predicate<string> Only(string fileName)
+                => f => string.Equals(fileName, f);
         }
     }
 }
