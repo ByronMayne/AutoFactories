77cbdbfedbe168eff6b9d3428424b0523ad1e1c8
Byron Mayne <Byronmayne@gmail.com>
2025-02-16 22:51:47 -0500

    Added a test to validate the shared factories create interfaces

---
+++ src/AutoFactories.Tests/Snapshots/GeneratedClassTests_HumanFactory.Shared_Factory_Creates_Interface#HumanFactory.verified.cs
@@ -0,0 +1,27 @@
+﻿// -----------------------------| Notes |-----------------------------
+// 1. The HumanFactory should also generate a shared IHumanFactory
+// -------------------------------------------------------------------
+public partial class HumanFactory
+{}
+// -------------------------------------------------------------------
+#nullable enable
+#pragma warning disable CS8019 // Unnecessary using directive.
+
+using AutoFactories;
+
+
+    public partial class HumanFactory : IHumanFactory
+    {
+        public HumanFactory()
+        {
+        }
+
+        /// <summary>
+        /// Creates a new instance of  <see cref="Human"/>
+        /// </summary>
+        public global::Human Create()
+        {
+            global::Human __result = new global::Human();
+            return __result;
+        }
+    }
---
+++ src/AutoFactories.Tests/Snapshots/GeneratedClassTests_IHumanFactory.Shared_Factory_Creates_Interface#IHumanFactory.verified.cs
@@ -0,0 +1,16 @@
+﻿// -----------------------------| Notes |-----------------------------
+// 1. The HumanFactory should also generate a shared IHumanFactory
+// -------------------------------------------------------------------
+#nullable enable
+#pragma warning disable CS8019 // Unnecessary using directive.
+
+using AutoFactories;
+
+
+    public interface IHumanFactory
+    {
+        /// <summary>
+        /// Creates a new instance of  <see cref="Human"/>
+        /// </summary>
+        global::Human Create();
+    }
